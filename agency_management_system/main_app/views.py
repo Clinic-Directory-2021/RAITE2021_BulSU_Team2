c845ddc7d11ee113932fc262ed467c867de5c929
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2021-10-23 14:38:02 +0800

    asdaf

--- agency_management_system/main_app/views.py
+++ agency_management_system/main_app/views.py
@@ -67,8 +67,8 @@ def add_crews_firebase(request):
         # #upload product image
         # storage.child(img_file_directory).put(crewImage, user['localId'])
 
-        doc_ref = db.collection('ship_crews').document(user['localId'])
-
+        doc_ref = db.collection('ship_crews').document()
+        
         # doc_ref_limitation = db.collection('ship_crew_limitation').document('aAYLxdGLmHVs3Yoo12au')
 
 
